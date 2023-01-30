d4b2f257a13f84dd064e67300c95984bb785e6fc
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2023-01-30 22:40:30 +0100

    init useReducer

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,59 +1,58 @@
 import React from "react";
 
-interface DataObj {
-  avgTotalVolume: number | undefined;
-  companyName: string | undefined;
-  symbol: string | undefined;
-  week52High: number | undefined;
-  week52Low: number | undefined;
-}
+const intitialState = {
+  dataName: "",
+  loading: false,
+  error: "",
+};
+
+const dataObjReducer = (state: any, action: any) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "FETCH_START":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { dataName: payload, loading: false };
+    case "FETCH_ERROR":
+      return { ...state, error: payload };
+    default:
+      return state;
+  }
+};
 
 export const Post = () => {
-  const [loading, setLoading] = React.useState(false);
-  const [dataObj, setDataObj] = React.useState<DataObj>({
-    avgTotalVolume: undefined,
-    companyName: undefined,
-    symbol: undefined,
-    week52High: undefined,
-    week52Low: undefined,
-  });
-  const [error, setError] = React.useState("");
+  const [data, dispatch] = React.useReducer(dataObjReducer, intitialState);
 
   const url =
     "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_6bdef7854f494c9cbeb36fb557cd0338";
 
   const handleFetch = () => {
-    setLoading(true);
+    dispatch({ type: "FETCH_START", loading: true });
+
     fetch(url)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        setLoading(false);
-        console.log(data);
-        setDataObj({
-          avgTotalVolume: data.avgTotalVolume,
-          companyName: data.companyName,
-          symbol: data.symbol,
-          week52High: data.week52High,
-          week52Low: data.week52Low,
+        dispatch({
+          type: "FETCH_SUCCESS",
+          payload: data.companyName,
         });
       })
       .catch((error) => {
-        setLoading(false);
-        setError(error);
+        dispatch({ type: "FETCH_ERROR", payload: error.toString() });
       });
   };
 
   return (
     <div>
       <button onClick={handleFetch}>
-        {loading ? "Wait..." : "Fetch the data"}
+        {data.loading ? "Wait..." : "Fetch the data"}
       </button>
-      {dataObj && !error ? (
-        <p>{JSON.stringify(dataObj)}</p>
+      {data.dataName && !data.error ? (
+        <p>{JSON.stringify(data.dataName)}</p>
       ) : (
-        <p>{error && "Something went wrong!"}</p>
+        <p>{data.error && data.error}</p>
       )}
     </div>
   );
